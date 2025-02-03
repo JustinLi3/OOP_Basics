47b85f257ed83feb7f050d9afb34ae788a005cd3
JustinLi3 <lijustin83@gmail.com>
2025-02-03 08:41:08 -0500

    init commit

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+   private String id; // Unique
+   private String name;
+
+   public Student(String id, String name){
+      this.id = id;
+      this.name = name;
+   }
+
+   // Temporary for testing code independently
+   public static void main(String[] args){
+
+   }
+}
